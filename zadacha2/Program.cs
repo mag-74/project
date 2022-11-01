9b6ec8ea85b59fcbedb2fae983ebf15192549751
m_a_g_ <m_a_g_12378@mail.ru>
2022-11-01 22:01:05 +0800

    save 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите Ваше имя");
+string username = Console.Readline();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
