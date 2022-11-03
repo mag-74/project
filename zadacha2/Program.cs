00d6618644285bb893c4806bc79d84bc26ebdd52
m_a_g_ <m_a_g_12378@mail.ru>
2022-11-03 20:05:37 +0800

    Программа отлаживается

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,5 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите Ваше имя");
-string username = Console.Readline();
-Console.WriteLine("Привет,");
-Console.WriteLine(username);
+﻿// Задачка по сортировке массива по возрастанию
+
+int[] Array = { 5, 8, 6, 4, 8, 15, 65, 2555, 12, 4586 };
+
+void SelectionSort(int[] Array);
+{
+    for (int i = 0; i < Array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < Array.Length; j++)
+        {
+            if (Array[j] < Array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temp = Array[i];
+        Array[i] = Array[maxPosition];
+        Array[maxPosition] = temp;
+    }
+}
+
+void printArray(int[] Array)
+{
+    int count = Array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+    Console.WriteLine();
+}
